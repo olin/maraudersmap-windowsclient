2539ea59b511a642159a304fd29209030d819ee4
Cory Dolphin <wcdolphin@gmail.com>
2012-11-16 22:28:23 -0500

    first commit

---
+++ Get Wireless Strengths/WirelessHelper.cs
@@ -0,0 +1,87 @@
+﻿//Adapted from Managed Wifi example by Cory Dolphin
+//Outputs Wireless Signal Strengths in JSON format, an array with many objects as described below:
+//EXAMPLE: [{"SSID":"OLIN_GUEST","BSSID":"00:15:E8:E3:DF","RSSI":"-78"}]
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using NativeWifi;
+
+namespace MaraudersMap
+{
+    class WirelessHelper
+    {
+        private WlanClient client;
+        public WirelessHelper()
+        {
+            client = new WlanClient();
+        }
+
+        public Dictionary<string, List<SSIDEntry>> GetWirelessStrengths()
+        {
+            Dictionary<string, List<SSIDEntry>> resultDict = new Dictionary<string, List<SSIDEntry>>();
+            foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
+            {
+                foreach (Wlan.WlanBssEntry network in wlanIface.GetNetworkBssList())
+                {
+                    int rss = network.rssi;
+                    byte[] macAddrBytes = network.dot11Bssid;
+
+                    string tMac = "";
+                    for (int i = 0; i < macAddrBytes.Length; i++)
+                    {
+                        tMac += macAddrBytes[i].ToString("x2").PadLeft(2, '0').ToUpper();
+                    }
+
+                    string macAddress = "";
+                    for (int i = 0; i < (tMac.Length / 2); i++)
+                    {
+                        macAddress += tMac.Substring(i * 2, 2);//note C# substring(i,j) goes from i to i+j
+                        if (i != 5) { macAddress += ":"; }
+                    }
+
+                    string ssid = System.Text.ASCIIEncoding.ASCII.GetString(network.dot11Ssid.SSID).ToString().Replace(((char)0) + "", ""); //replace null chars
+                    if(resultDict.ContainsKey(ssid))
+                        resultDict[ssid].Add(new SSIDEntry(ssid, macAddress, rss.ToString()));
+                }
+            }
+
+            return resultDict;
+        }
+        static void Main(string[] args)
+        {
+            WirelessHelper wclient = new WirelessHelper();
+            Dictionary<string, List<SSIDEntry>> wirelessList = wclient.GetWirelessStrengths();
+            Console.Write("[");
+            foreach (List<SSIDEntry> ssids in wirelessList.Values)
+            {
+                foreach (SSIDEntry ssid in ssids)
+                {
+                    Console.Write(ssid.ToString());
+                }
+            }
+            Console.Write("]");
+
+        }
+    }
+    public class SSIDEntry
+    {
+        private string SSID, MACAddress, RSSI;
+        public SSIDEntry(string SSID, string MACAddress, string RSSI)
+        {
+            this.SSID = SSID;
+            this.MACAddress = MACAddress;
+            this.RSSI = RSSI;
+        }
+
+
+        public String ToString()
+        {
+            return String.Format("{'ssid':'{0}','mac':'{1}','rssi':'{2}'}", SSID,MACAddress,RSSI);
+        }
+    }
+
+}
---
+++ MaraudersMap/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Linq;
+
+
+
+using Controls = Elysium.Theme.Controls;
+using System.Windows;
+
+
+namespace MaraudersMap
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Controls.Window
+	{
+        private System.Windows.Forms.NotifyIcon notifier;
+        private System.Windows.Forms.ContextMenuStrip contextMenu1;
+        private System.Windows.Forms.ToolStripMenuItem refreshItem, correctItem, currentPositionItem, exitItem;
+        private Timer refreshTimer;
+        private int currentPosition = 0;
+
+		public MainWindow()
+		{
+            InitializeComponent();
+			// Insert code required on object creation below this point.
+            notifier = new System.Windows.Forms.NotifyIcon();
+            this.contextMenu1 = new System.Windows.Forms.ContextMenuStrip();
+            refreshItem = new System.Windows.Forms.ToolStripMenuItem("Refresh");
+            correctItem = new System.Windows.Forms.ToolStripMenuItem("Correct Position");
+            currentPositionItem = new System.Windows.Forms.ToolStripMenuItem("Current Position");
+            exitItem = new System.Windows.Forms.ToolStripMenuItem("Exit");
+            this.contextMenu1.Items.AddRange(new System.Windows.Forms.ToolStripMenuItem[] { refreshItem, correctItem, currentPositionItem, exitItem });
+
+            refreshItem.Click += new System.EventHandler(this.refreshItem_Click);
+            correctItem.Click += new System.EventHandler(this.correcPositiontItem_Click);
+            exitItem.Click += new System.EventHandler(this.exitItem_Click);
+            notifier.Icon = MaraudersMap.Properties.Resources.TaskbarIconHigh;
+            notifier.Visible = true;
+            notifier.Click += new EventHandler(notifyIcon1_Click);
+            contextMenu1.LostFocus += (object sender, EventArgs a) => { contextMenu1.Hide(); };
+            refreshTimer = new Timer(checkPosition, null, TimeSpan.Zero, TimeSpan.FromMinutes(1));
+
+        
+        }
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == System.Windows.WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
+        }
+
+        void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            contextMenu1.Show(System.Windows.Forms.Cursor.Position.X, System.Windows.Forms.Cursor.Position.Y);
+        }
+        private void refreshItem_Click(object Sender, EventArgs e)
+        {
+        }
+        private void correcPositiontItem_Click(object Sender, EventArgs e)
+        {
+            currentPositionItem.Text = "Clicked";
+        }
+        private void exitItem_Click(object Sender, EventArgs e)
+        {
+            this.Close();
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            notifier.Dispose();
+        }
+
+        private void checkPosition(object state)
+        {
+            currentPosition += 1;
+            currentPositionItem.Text = "" + currentPosition;
+        }
+
+        private void timeSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            string message = "Refresh every ";
+            if(e.NewValue <= 1){
+                message += "minute" + ":";
+            }
+            else{
+                message += e.NewValue + " minutes";            
+            }
+
+            if(this.RefreshTimeBlock != null)
+                this.RefreshTimeBlock.Text = message;
+            refreshTimer.Change(TimeSpan.Zero, TimeSpan.FromMinutes(e.NewValue));
+        }
+
+        private void LoginButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (this.UsernameInput.Text.Length > 0 && this.PasswordInput.Password.Length > 0)
+            {
+                //Let's go ahead and login
+            }
+            else
+            {
+                //Let's throw a warning message
+            }
+        }
+
+	}
+}
